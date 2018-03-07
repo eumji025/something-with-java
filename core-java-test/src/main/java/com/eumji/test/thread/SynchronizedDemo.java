8302479c880dde6b39027287616bc808d85456c4
eumji025 <eumji025@gmail.com>
2018-03-07 23:36:52 +0800

    对三种synchronized关键字的进一步测试。看其影响范围

--- core-java-test/src/main/java/com/eumji/test/thread/SynchronizedDemo.java
+++ core-java-test/src/main/java/com/eumji/test/thread/SynchronizedDemo.java
@@ -8,7 +8,7 @@ package com.eumji.test.thread;
  *
  * 而静态同步方法只能锁类
  *
- * 同步方法只能锁当前对象。
+ * 同步方法只能锁当前对象,同时还可以获取类锁（调用类静态方法时候会阻塞其他类锁）。
  *
  * @email eumji025@gmail.com
  * @author: EumJi
@@ -21,8 +21,9 @@ public class SynchronizedDemo {
 
         public static synchronized void hello1() throws InterruptedException {
 
-            System.out.println("ok!!!!!");
+            System.out.println("ok!!!!!11111");
             Thread.sleep(3000L);
+            System.out.println("hello ending 1111");
         }
 
         public static synchronized void hello5() throws InterruptedException {
@@ -33,7 +34,8 @@ public class SynchronizedDemo {
 
         public synchronized void hello2() throws InterruptedException {
             System.out.println("ok2!!!!");
-            Thread.sleep(3000L);
+            Thread.sleep(1000L);
+            hello1();
         }
 
         public synchronized void hello4() throws InterruptedException {
@@ -56,9 +58,39 @@ public class SynchronizedDemo {
         }
     }
 
-    public static void main(String[] args) {
-
+    /**
+     * 测试对象锁+类锁 会影响其他的类锁
+     * @throws InterruptedException
+     */
+    public static void objectAndClass() throws InterruptedException {
         MehthodDemo mehthodDemo = new MehthodDemo();
+        new Thread(()->{
+            try {
+                mehthodDemo.hello2();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        Thread.sleep(500L);
+        new Thread(()->{
+            try {
+                MehthodDemo.hello1();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    /**
+     * 类锁和同步块，对象同步锁之间区别和关联
+     * 同步快可以锁住对象也可以锁类，非常灵活，范围可控制
+     * 同步方法也是可以重入类锁的，，，需要注意
+     * 类锁只能锁类，不影响对象
+     */
+    public static void lockTest() {
+        MehthodDemo mehthodDemo = new MehthodDemo();
+
         new Thread(()-> {
             try {
                 MehthodDemo.hello1();
@@ -67,13 +99,13 @@ public class SynchronizedDemo {
             }
         }).start();
 
-//        new Thread(()-> {
-//            try {
-//                mehthodDemo.hello33();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }).start();
+        new Thread(()-> {
+            try {
+                mehthodDemo.hello33();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
 
         new Thread(()-> {
             try {
@@ -82,5 +114,13 @@ public class SynchronizedDemo {
                 e.printStackTrace();
             }
         }).start();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        objectAndClass();
+
+
+
     }
 }
