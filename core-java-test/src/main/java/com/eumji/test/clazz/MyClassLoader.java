db9059ac5b1b9ace78bf9c10da8a3e86c4d40965
eumji025 <eumji025@gmail.com>
2019-06-08 16:32:38 +0800

    sonar fixed

--- core-java-test/src/main/java/com/eumji/test/clazz/MyClassLoader.java
+++ core-java-test/src/main/java/com/eumji/test/clazz/MyClassLoader.java
@@ -1,16 +1,21 @@
 package com.eumji.test.clazz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 
 /**
  * 自定义classLoader，具体描述参照ClassLoader doc文档描述的内容来进行
  * 这里最关键的地方在于重写findClass方法，并且需要调用defineClass来进行定义。最后如果要实例化则使用newInstance方法
+ *
  * @email eumji025@gmail.com
  * @author:EumJi
  * @date: 2019/4/21
  * @time: 12:54
  */
 public class MyClassLoader extends ClassLoader {
+    private static Logger logger = LoggerFactory.getLogger(MyClassLoader.class);
 
     private String classLoaderName;
     private String fileSuffix = ".class";
@@ -29,34 +34,39 @@ public class MyClassLoader extends ClassLoader {
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         try {
             byte[] classData = findClassData(name);
-            return defineClass(name,classData,0,classData.length);
+            return defineClass(name, classData, 0, classData.length);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("find data error:{}",e.getMessage());
+
         }
         return super.findClass(name);
     }
 
     /**
      * 根据名称获取class的字节数组
+     *
      * @param name
      * @return
      */
     private byte[] findClassData(String name) throws IOException {
-        name = name.replace(".","/");
-        if (!name.endsWith(fileSuffix)){
-            name= name+fileSuffix;
+        name = name.replace(".", File.separator);
+        if (!name.endsWith(fileSuffix)) {
+            name = name + fileSuffix;
         }
-        String basePath = MyClassLoader.class.getClass().getResource("/").getPath()+"/";
-        File file = new File(basePath+name);
-        FileInputStream fileInputStream = new FileInputStream(file);
-        byte[] tmp = new byte[1024];
-        int length = -1;
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        while ((length = fileInputStream.read(tmp)) > -1){
-            byteArrayOutputStream.write(tmp,0,length);
-        }
+        String basePath = MyClassLoader.class.getResource("/").getPath() + File.separator;
+        File file = new File(basePath + name);
+        try (
+                FileInputStream fileInputStream = new FileInputStream(file);
+        ) {
+            byte[] tmp = new byte[1024];
+            int length = -1;
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            while ((length = fileInputStream.read(tmp)) > -1) {
+                byteArrayOutputStream.write(tmp, 0, length);
+            }
 
-        return byteArrayOutputStream.toByteArray();
+            return byteArrayOutputStream.toByteArray();
+        }
     }
 
 
@@ -70,8 +80,8 @@ public class MyClassLoader extends ClassLoader {
     public static void main(String[] args) throws ClassNotFoundException {
         ClassLoader myclassloader = new MyClassLoader("myclassloader");
         Class<?> aClass = myclassloader.loadClass("com.eumji.test.clazz.Hello");
-        System.out.println(aClass);
-        System.out.println(aClass.getClassLoader());
+        logger.info("{}",aClass);
+        logger.info("{}",aClass.getClassLoader());
     }
 
 }
